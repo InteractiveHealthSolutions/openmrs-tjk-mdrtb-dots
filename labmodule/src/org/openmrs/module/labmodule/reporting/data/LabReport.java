2b361148a13350ec196d27a8ad6ae23adc55d2a2
Rabbia <Rabbia@199.172.1.123>
2016-08-29 13:54:16 +0500

    29/8

---
+++ labmodule/src/org/openmrs/module/labmodule/reporting/data/LabReport.java
@@ -0,0 +1,209 @@
+package org.openmrs.module.labmodule.reporting.data;
+
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.Cohort;
+import org.openmrs.Concept;
+import org.openmrs.Location;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.labmodule.TbConcepts;
+import org.openmrs.module.labmodule.exception.MdrtbAPIException;
+import org.openmrs.module.labmodule.reporting.ReportSpecification;
+import org.openmrs.module.labmodule.reporting.ReportUtil;
+import org.openmrs.module.labmodule.service.TbService;
+import org.openmrs.module.labmodule.specimen.LabResult;
+import org.openmrs.module.labmodule.specimen.reporting.Oblast;
+import org.openmrs.module.reporting.ReportingConstants;
+import org.openmrs.module.reporting.cohort.definition.AgeCohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.GenderCohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.service.CohortDefinitionService;
+import org.openmrs.module.reporting.common.DateUtil;
+import org.openmrs.module.reporting.dataset.MapDataSet;
+import org.openmrs.module.reporting.dataset.definition.CohortCrossTabDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.evaluation.parameter.ParameterizableUtil;
+import org.openmrs.module.reporting.report.ReportData;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.reporting.report.definition.service.ReportDefinitionService;
+import org.openmrs.module.reporting.report.renderer.RenderingMode;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * LAB Report
+ */
+public class LabReport implements ReportSpecification {
+	
+	/**
+	 * @see ReportSpecification#getName()
+	 */
+	public String getName() {
+		return Context.getMessageSourceService().getMessage("labmodule.labReports");
+	}
+	
+	/**
+	 * @see ReportSpecification#getDescription()
+	 */
+	public String getDescription() {
+		return Context.getMessageSourceService().getMessage("labmodule.report.description");
+	}
+	
+	@ModelAttribute("labResults")
+	public Collection<Oblast> getLabResults() {
+		
+		Location location = null;
+		return Context.getService(TbService.class).getOblasts();
+		
+	}
+	
+	/**
+	 * @see ReportSpecification#getParameters()
+	 */
+	public List<Parameter> getParameters() {
+		List<Parameter> l = new ArrayList<Parameter>();
+		l.add(new Parameter("location", Context.getMessageSourceService().getMessage("dotsreports.facility"), Location.class));
+		l.add(new Parameter("year", Context.getMessageSourceService().getMessage("dotsreports.year"), Integer.class));
+		return l;
+	}
+	
+	/**
+	 * @see ReportSpecification#getRenderingModes()
+	 */
+	public List<RenderingMode> getRenderingModes() {
+		List<RenderingMode> l = new ArrayList<RenderingMode>();
+		l.add(ReportUtil.renderingModeFromResource("HTML", "org/openmrs/module/labmodule/reporting/data/output/DOTS-TB07-OMAR" + 
+			(StringUtils.isNotBlank(Context.getLocale().getLanguage()) ? "_" + Context.getLocale().getLanguage() : "") + ".html"));
+		return l;
+	}
+	
+	
+
+	/**
+	 * @see ReportSpecification#validateAndCreateContext(Map)
+	 */
+	public EvaluationContext validateAndCreateContext(Map<String, Object> parameters) {
+		
+		EvaluationContext context = ReportUtil.constructContext(parameters);
+		Integer year = (Integer)parameters.get("year");
+		
+		if (year == null) {
+			throw new IllegalArgumentException(Context.getMessageSourceService().getMessage("dotsreports.error.pleasEnterAYear"));
+		}
+		context.getParameterValues().putAll(ReportUtil.getPeriodDates(year, null, null));
+		
+		Map<String,Object>pMap = context.getParameterValues();
+		Set<String> keySet = pMap.keySet();
+		
+		return context;
+	}
+	
+	/**
+	 * ReportSpecification#evaluateReport(EvaluationContext)
+	 */
+	@SuppressWarnings("unchecked")
+	public ReportData evaluateReport(EvaluationContext context) {
+		
+		Location location = (Location) context.getParameterValue("location");
+		//Location location = (Location) context.getParameterValue("location");
+		// Just testing... GIT is not letting me commit this file :/
+		String oblast = (String) context.getParameterValue("oblast");
+		Date endDate = (Date)context.getParameterValue("endDate");
+		Date startDate = (Date)context.getParameterValue("startDate");
+		
+		Oblast o = null;
+		if(!oblast.equals("") && location == null)
+			o =  Context.getService(TbService.class).getOblast(Integer.parseInt(oblast));
+		
+		List<Location> locList = new ArrayList<Location>();
+		if(o != null && location == null)
+			locList = Context.getService(TbService.class).getLocationsFromOblastName(o);
+		else if (location != null)
+			locList.add(location);
+		
+		if(location != null)
+			context.addParameterValue("location", location.getName()); 
+		else if(o != null)
+			context.addParameterValue("location", o.getName()); 
+		else
+			context.addParameterValue("location", "All"); 
+		
+		ReportDefinition report = new ReportDefinition(); 
+		
+		if (!locList.isEmpty()){
+			List<CohortDefinition> cohortDefinitions = new ArrayList<CohortDefinition>();
+			for(Location loc : locList)
+				cohortDefinitions.add(Cohorts.getLocationFilter(loc, startDate, endDate));
+				
+			if(!cohortDefinitions.isEmpty()){
+				report.setBaseCohortDefinition(ReportUtil.getCompositionCohort("OR", cohortDefinitions), null);
+			}
+		}
+		
+		CohortDefinition allLabResults = Cohorts.getAllLabResultDuring(startDate,endDate);
+		
+		Cohort test = null;
+		try {
+					test = Context.getService(CohortDefinitionService.class).evaluate(allLabResults, new EvaluationContext());
+				} catch (EvaluationException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+				
+				System.out.println("********MEMBERS**************");
+				Set<Integer> ids = test.getMemberIds();
+				for(Integer testid:ids) {
+					System.out.println(testid);
+				}
+		
+		   
+		CohortCrossTabDataSetDefinition d = new CohortCrossTabDataSetDefinition(); 
+		d.addParameter(ReportingConstants.END_DATE_PARAMETER); 
+		   
+		//d.addRow("total", allLabResults, null);
+		   
+		d.addColumn("total", allLabResults, null); 
+		  
+		report.addDataSetDefinition("all", d, null); 
+		   
+		ReportData results = null;
+		  
+		try {
+			results = Context.getService(ReportDefinitionService.class).evaluate(report, context);
+		} catch (EvaluationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return results;
+	}
+
+}
