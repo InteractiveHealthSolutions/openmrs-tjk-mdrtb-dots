5d5da3231ad6a262577c6f5540c344fa55c2932a
Rabbia <Rabbia@199.172.1.238>
2016-09-19 17:05:51 +0500

    19/9/2016 Commit

--- labmodule/web/src/org/openmrs/module/labmodule/web/controller/reporting/LabPatientListController.java
+++ labmodule/web/src/org/openmrs/module/labmodule/web/controller/reporting/LabPatientListController.java
@@ -13,10 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang.StringUtils;
 import org.apache.poi.hssf.util.HSSFColor.AQUA;
 import org.openmrs.Cohort;
+import org.openmrs.Concept;
 import org.openmrs.Location;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.labmodule.TbUtil;
 import org.openmrs.module.labmodule.service.TbService;
+import org.openmrs.module.labmodule.specimen.Culture;
 import org.openmrs.module.labmodule.specimen.LabResult;
 import org.openmrs.module.labmodule.specimen.reporting.Oblast;
 import org.springframework.stereotype.Controller;
@@ -37,6 +39,19 @@ public class LabPatientListController extends AbstractListController{
             		@RequestParam(value="lab", required=false) Location lab,
             		@RequestParam(value="date_from", required=false) Date dateFrom,
             		@RequestParam(value="date_to", required=false) Date dateTo,
+            		@RequestParam(value="requestingLabName", required=false) Concept requestingLabName,
+            		@RequestParam(value="investigationPurpose", required=false) Concept investigationPurpose,
+            		@RequestParam(value="biologicalSpecimen", required=false) Concept biologicalSpecimen,
+            		@RequestParam(value="peripheralLabNo", required=false) String peripheralLabNo,
+            		@RequestParam(value="microscopyResult", required=false) Concept microscopyResult,
+            		@RequestParam(value="result_from", required=false) Date resultDateFrom,
+            		@RequestParam(value="result_to", required=false) Date resultDateTo,
+            		@RequestParam(value="peripheral", required=false) String[] peripheral,
+            		@RequestParam(value="microscopy", required=false) String[] microscopy,
+            		@RequestParam(value="xpert", required=false) String[] xpert,
+            		@RequestParam(value="hain", required=false) String[] hain,
+            		@RequestParam(value="hain2", required=false) String[] hain2,
+            		@RequestParam(value="culture", required=false) String[] culture,
             		ModelMap model) {
     	
     	if(oblast != null){
@@ -70,15 +85,51 @@ public class LabPatientListController extends AbstractListController{
 	        	    if(!gender.equals("") && !lr.getPatient().getGender().equals(gender))
 	        	    	continue;
 	        	    
-	        	    if(!lab.equals("") || lab != null) 
-	        	    	if(lr.getLocation() != lab)
-	        	    		continue;
+	        	    if(lab != null && lr.getLocation() != lab)
+	        	    	continue;
 	        	    
-	        	    if(dateFrom != null &&  dateFrom.before(lr.getDateCollected()))
+	        	    if(dateFrom != null &&  dateFrom.after(lr.getDateCollected()))
 	        	    	continue;
 	        	    
 	        	    if(dateTo != null &&  dateTo.before(lr.getDateCollected()))
 	        	    	continue;
+	        	    
+	        	    if(requestingLabName != null && requestingLabName != lr.getRequestingLabName())
+	        	    	continue;
+	        	    
+	        	    if(investigationPurpose != null && investigationPurpose != lr.getInvestigationPurpose())
+	        	    	continue;
+	        	    
+	        	    if(biologicalSpecimen != null && biologicalSpecimen != lr.getBiologicalSpecimen())
+	        	    	continue;
+	        	    
+	        	    if(!peripheralLabNo.equals("") && !peripheralLabNo.equals(lr.getPeripheralLabNumber()))
+	        	    	continue;
+	        	    
+	        	    if(microscopyResult != null && microscopyResult != lr.getMicroscopyResult())
+	        	    	continue;
+	        	    
+	        	    if(resultDateFrom != null &&  resultDateFrom.after(lr.getDateResult()))
+	        	    	continue;
+	        	    
+	        	    if(resultDateTo != null &&  resultDateTo.before(lr.getDateResult()))
+	        	    	continue;
+	        	    
+	        	    List<Culture> cultures = lr.getCultures();
+	        	    if(culture != null && cultures.size() == 0)
+	        	    	continue;
+	        	    else{
+	        	    	
+	        	    	Date date = null;
+	        	    	int i = 0;
+	        	    	/*for(int j = 0; i < cultures.size(); i++){
+	        	    		
+	        	    		if(date != null && date.iscultures.get(j).getResultDate())
+	        	    		
+	        	    	}*/
+	        	    		
+	        	    	
+	        	    }
 		        	
 		        	mMap.put("patientId", lr.getPatient().getId());
 		        	mMap.put("encounterId", lr.getId());
@@ -92,6 +143,12 @@ public class LabPatientListController extends AbstractListController{
 		            mMap.put("requestingMedicalFacility", lr.getRequestingLabName() == null ? "" : lr.getRequestingLabName().getName());
 		            mMap.put("investigationPurpose", lr.getInvestigationPurpose() == null ? "" : lr.getInvestigationPurpose().getName());
 		            mMap.put("biologicalSpecimen", lr.getBiologicalSpecimen() == null ? "" : lr.getBiologicalSpecimen().getName());
+		            
+		            mMap.put("peripheralLabNo", lr.getPeripheralLabNumber() == null ? "" : lr.getPeripheralLabNumber());
+		            mMap.put("microscopyResult", lr.getMicroscopyResult() == null ? "" : lr.getMicroscopyResult().getName());	
+		            mMap.put("dateResult", lr.getDateResult() == null ? "" : df.format(lr.getDateResult()));
+		            
+		            
 		            
 		            data.add(mMap);
 		        }
@@ -116,14 +173,34 @@ public class LabPatientListController extends AbstractListController{
 		        	    if(!gender.equals("") && !lr.getPatient().getGender().equals(gender))
 		        	    	continue;
 		        	    
-		        	    if(!lab.equals("") || lab != null)
-		        	    		if(lr.getLocation() != lab)
-		        	    			continue;
-		        	    
-		        	    if(dateFrom != null &&  dateFrom.before(lr.getDateCollected()))
+		        	    if(lab != null && lr.getLocation() != lab)
 		        	    	continue;
 		        	    
-		        	    if(dateTo != null &&  dateTo.after(lr.getDateCollected()))
+		        	    if(dateFrom != null &&  (dateFrom.after(lr.getDateCollected()) || dateFrom.equals(lr.getDateCollected())))
+		        	    	continue;
+		        	    
+		        	    if(dateTo != null && ( dateTo.before(lr.getDateCollected()) || dateTo.equals(lr.getDateCollected())))
+		        	    	continue;
+		        	    
+		        	    if(requestingLabName != null && requestingLabName != lr.getRequestingLabName())
+		        	    	continue;
+		        	    
+		        	    if(investigationPurpose != null && investigationPurpose != lr.getInvestigationPurpose())
+		        	    	continue;
+		        	    
+		        	    if(biologicalSpecimen != null && biologicalSpecimen != lr.getBiologicalSpecimen())
+		        	    	continue;
+		        	    
+		        	    if(!peripheralLabNo.equals("") && !peripheralLabNo.equals(lr.getPeripheralLabNumber()))
+		        	    	continue;
+		        	    
+		        	    if(microscopyResult != null && microscopyResult != lr.getMicroscopyResult())
+		        	    	continue;
+		        	    
+		        	    if(resultDateFrom != null &&  resultDateFrom.after(lr.getDateResult()))
+		        	    	continue;
+		        	    
+		        	    if(resultDateTo != null &&  resultDateTo.before(lr.getDateResult()))
 		        	    	continue;
 			        	
 			        	mMap.put("patientId", lr.getPatient().getId());
@@ -139,12 +216,30 @@ public class LabPatientListController extends AbstractListController{
 			            mMap.put("investigationPurpose", lr.getInvestigationPurpose() == null ? "" : lr.getInvestigationPurpose().getName());
 			            mMap.put("biologicalSpecimen", lr.getBiologicalSpecimen() == null ? "" : lr.getBiologicalSpecimen().getName());
 			            
+			            mMap.put("peripheralLabNo", lr.getPeripheralLabNumber() == null ? "" : lr.getPeripheralLabNumber());
+			            mMap.put("microscopyResult", lr.getMicroscopyResult() == null ? "" : lr.getMicroscopyResult().getName());	
+			            mMap.put("dateResult", lr.getDateResult() == null ? "" : df.format(lr.getDateResult()));
+			            
 			            data.add(mMap);
 			        }
 	        		
 	        	}
 	        	
 	        }
+	        
+	        if(peripheral != null)
+	        	model.addAttribute("peripheral", "yes");
+	        if(microscopy != null)
+	        	model.addAttribute("microscopy", "yes");
+	        if(xpert != null)
+	        	model.addAttribute("xpert", "yes");
+	        if(hain != null)
+	        	model.addAttribute("hain", "yes");
+	        if(hain2 != null)
+	        	model.addAttribute("hain2", "yes");
+	        if(culture != null)
+	        	model.addAttribute("culture", "yes");
+	        
 	        model.addAttribute("data", data);
         }
     	
